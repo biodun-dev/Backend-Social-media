f885f751d3d33d20cd98fb68dde5b64d9c0c86bf
biodun-code <abdultoheeb97@gmail.com>
2024-07-07 14:01:11 +0100

    test

--- src/middlewares/mockAuth.ts
+++ src/middlewares/mockAuth.ts
@@ -1,18 +1,16 @@
-
+// src/middlewares/mockAuth.ts
 import { Request, Response, NextFunction } from 'express';
 import { Types } from 'mongoose';
 
 const mockAuth = (req: Request, res: Response, next: NextFunction) => {
-    console.log("Using mock authentication for testing");
+  console.log("Using mock authentication for testing");
   req.user = {
-    _id: new Types.ObjectId(),
+    id: new Types.ObjectId().toString(), // Ensure this matches your User model's ID type
     username: 'testUser',
     email: 'test@example.com',
-    password: 'password123',
     following: [],
   } as any; // Cast to any to satisfy the User type in your global declaration
   next();
 };
 
 export default mockAuth;
-
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -12,11 +12,16 @@ import {
   loginValidationRules,
   validate,
 } from "../middlewares/validationMiddleware";
+import { conditionalMiddleware } from "../utils/conditionalMiddleware";
 const router = express.Router();
 
 router.post("/register", logRequest, userValidationRules(), validate, register);
 router.post("/login", logRequest, loginValidationRules(), validate, login);
-router.post("/follow/:followId", logRequest, authenticate, followUser);
-router.get("/me/data", logRequest, authenticate, getUserData);
+router.post(
+  "/follow/:followId",
+  conditionalMiddleware(authenticate),
+  followUser
+);
+router.get("/me/data", conditionalMiddleware(authenticate), getUserData);
 
 export default router;
